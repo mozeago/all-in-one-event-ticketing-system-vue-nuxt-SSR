06dc0a158c16c94d82ed6046c3a3657c5d52533a
Moses Asiago <mozzeago@gmail.com>
2022-09-18 02:33:31 +0300

    initial commit, vue-nuxt can fetch from flask backend

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    {{greeting}}
+    {{flaskGreeting}}
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data: function () {
+    return {
+      greeting: "Hello Vue !",
+      flaskGreeting: ""
+    }
+  },
+  created: async function () {
+    const gResponse = await fetch("http://127.0.0.1:5000/greeting");
+    const gObject = await gResponse.json()
+    this.flaskGreeting=gObject.greeting
+  }
+}
+</script>
